782b554042f95c391a4e2de9095be826de525d76
ajaykumar302 <59527426+ajaykumar302@users.noreply.github.com>
2023-01-09 00:11:54 +0530

    Update index.jsx

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -24,8 +24,8 @@ const Main = () => {
 					<li><Link to="/intenship" className={styles.link}>AI INTENSHIP</Link></li>
 					<li><Link to="/career" className={styles.link}>CAREER</Link></li>
 					<li><Link to="/blog" className={styles.link}>BLOG</Link></li>
-					<li><Link to="/about" className={styles.link}>ABOUT</Link></li>
 					<li><Link to="/contactus" className={styles.link}>CONTACT US</Link></li>
+					<li><Link to="/admin" className={styles.link}>ADMIN</Link></li>	
 					<button className={styles.white_btn} onClick={handleLogout}>
 					Logout
 				</button>
